d0e4d084cadd641957802f926476b3596b8d7515
marijan10marijan <mmarijan39@gmail.com>
2024-04-07 17:38:50 +0200

    changes

--- src/components/oNama/ONamaPrica.jsx
+++ src/components/oNama/ONamaPrica.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./oNama.module.css";
+import Image from "next/image";
 
 const ONamaPrica = () => {
   return (
@@ -19,6 +20,37 @@ const ONamaPrica = () => {
           </strong>
         </p>
       </div>
+      <div className={styles.nasaPricaBoxTwo}>
+        <div className={styles.nasaPricaBoxTwoImage}>
+          <Image
+            src={"https://zaposlise.hr/wp-content/uploads/2021/11/img1.jpg"}
+            alt="slika poslovnog sastanka"
+            fill
+          />
+        </div>
+        <div className={styles.nasaPricaBoxTwoContent}>
+          <h3>Što nas pokreće?</h3>
+          <p>
+            Vjerujemo da je idealan posao današnjice ipak malo više od plaće i
+            da sam proces pronalaženja onog savršenog spoja između poslodavca i
+            zaposlenika treba biti jednostavan i ali i dostojanstven.
+          </p>
+          <p>
+            Istražili smo pregršt alata i aplikacije za pronalaženje talenata
+            koje trenutno koriste tvrtke u RH. Direktna komunikacija je naša
+            najbolja praksa za obostrano zadovoljstvo i kvalitetnu poslovnu
+            suradnju. Prilagodili smo sve procese novim generacijama koje rade u
+            “petoj brzini”.
+          </p>
+          <p>
+            Naš cilj je postati promjena na tržištu rada, ukloniti formalnosti i
+            barijere, stvarati nove veze i prilike.
+          </p>
+          <p>Mi smo vaša podrška na ovom novom putovanju.</p>
+        </div>
+        <div className={styles.nasaPricaBoxTwoDecoration}></div>
+      </div>
+      <div className={styles.nasaPricaDecoration}></div>
     </main>
   );
 };
